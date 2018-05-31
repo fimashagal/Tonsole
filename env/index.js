e5c0cbb11f8f12eb5109cbed2e9fc2cd3b5102cc
fox-pc\user <fimashagal@gmail.com>
2018-05-31 20:03:01 +0300

    upd

--- env/index.js
+++ env/index.js
@@ -1,5 +1,4 @@
 "use strict";
-alert(222);
 define(function (require) {
 
     const req = {
@@ -9,14 +8,11 @@ define(function (require) {
     };
 
     let Tonsole = req.Tonsole();
+
     main();
 
     function main() {
-
-        (() => {
-
-            Tonsole.initialize();
-        })();
+        (Tonsole.initialize)();
     }
 
 });
